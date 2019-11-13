abc9e8b2914f11b5e0c3be97084ed9ff9e86f230
Ivan <marionltan@gmail.com>
2019-11-13 14:44:33 +0800

    changed put to patch in the EditStream action Creator

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,18 +1,27 @@
+import _ from 'lodash'
 import React from 'react';
 import {connect} from 'react-redux'
-import {fetchStream} from '../../actions'
+import {fetchStream,editStream} from '../../actions'
+import StreamForm from './StreamForm';
+
 class StreamEdit extends React.Component{
 	componentDidMount(){
 		this.props.fetchStream(this.props.match.params.id)
+	}
+	onSubmit=(fromValues)=>{
+		this.props.editStream(this.props.match.params.id, fromValues)
 	}
 	render(){
 		//check if this.ptops.stream is null
 		if(!this.props.stream){
 			return <div>Loading...</div>
 		}
+		// redux form passes the initialValues props to StreamForm. StreamForm has the fields with the same title
+		//this.props.stream has the title
 		return (
 			<div>
-				{this.props.stream.title}
+				<h3>Edit a stream</h3>
+				<StreamForm initialValues={_.pick(this.props.stream,'title','description')} onSubmit={this.onSubmit}/>
 			</div>
 		);}
 };
@@ -22,4 +31,4 @@ const mapStateToProps=(state,ownProps)=>{
 	return{stream: state.streams[ownProps.match.params.id]};
 };
 
-export default connect(mapStateToProps,{fetchStream})(StreamEdit);
+export default connect(mapStateToProps,{fetchStream,editStream})(StreamEdit);
