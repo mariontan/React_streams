ca052dcc252b485acb08c4bef76cb6f7673fa1d3
Ivan <marionltan@gmail.com>
2019-11-13 10:37:18 +0800

    can render a list of stream tittle and decription

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {signIn,signOut} from '../actions'
 
 class GoogleAuth extends React.Component{
-
-	state = { isSignedIn: null};
 
 	componentDidMount(){
 		window.gapi.load('client:auth2',()=>{
@@ -11,14 +11,19 @@ class GoogleAuth extends React.Component{
 				scope:'email'
 			}).then(()=>{
 				this.auth=window.gapi.auth2.getAuthInstance();
-				this.setState({isSignedIn: this.auth.isSignedIn.get()})
+
+				this.onAuthChange(this.auth.isSignedIn.get());
 				this.auth.isSignedIn.listen(this.onAuthChange);
 			});
 		});
 	}
-	onAuthChange=()=>{
-		this.setState({isSignedIn: this.authisSignedIn.get()});
-	}
+	onAuthChange=(isSignedIn)=>{
+		if(isSignedIn){
+			this.props.signIn(this.auth.currentUser.get().getId());
+		}else{
+			this.props.signOut();
+		}
+	};
 	onSignInClick=()=>{
 		this.auth.signIn();
 	};
@@ -26,9 +31,9 @@ class GoogleAuth extends React.Component{
 		this.auth.signOut();
 	};
 	renderAuthButton(){
-		if(this.state.isSignedIn === null){
+		if(this.props.isSignedIn === null){
 			return null;
-		}else if(this.state.isSignedIn){
+		}else if(this.props.isSignedIn){
 			return(
 				<button onClick={this.onSignOutClick} className="ui red google button">
 					<i className="google icon"/>
@@ -51,4 +56,8 @@ class GoogleAuth extends React.Component{
 	};
 };
 
-export default GoogleAuth;
+const mapStateToProps =(state)=>{
+	return {isSignedIn:state.auth.isSignedIn}
+}
+
+export default connect(mapStateToProps,{signIn,signOut})(GoogleAuth);
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {Field, reduxForm} from 'redux-form';
+import component from 'react-redux';
+import {createStream} from '../../actions'
 
-const StreamCreate=()=>{
-	return <div>StreamCreate</div>;
+class StreamCreate extends React.Component{
+	renderError({error,touched}){
+		if(touched && error){
+			return(
+				<div className-="ui error message">
+					<div className="header">{error}</div>
+				</div>
+			);
+		}
+	}
+	renderInput=({input,label, meta})=>{
+		const className= `filed${meta.error && meta.touched ? 'error':''}`;
+		return (
+			<div className={className}>
+				<label>{label}</label>
+				<input {...input} autoComplete="off"/>
+				{this.renderError(meta)}
+			</div>
+		);
+	}
+	onSubmit=(formValues)=>{
+		this.props.createStream(formValues);
+	}
+	render(){
+		return(
+			<form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+				<Field name ="title" component={this.renderInput} label="Enter Titile"/>
+				<Field name ="description"component={this.renderInput} label="Enter Description"/>
+				<button className="ui button primary">Submit</button>
+			</form>
+		);
+	}
+}
+//gets called each time the user interacts with the form
+const validate =(formValues)=>{
+	const errors ={};
+
+	if(!formValues.title){
+		errors.title = 'You must enter a title';
+	}
+	if(!formValues.description){
+		errors.description='enter description'
+	}
+	return errors;
 };
 
-export default StreamCreate;
+
+const formWrapped =  reduxForm({
+	form: 'StreamCreate',
+	validate: validate
+})(StreamCreate);
+
+export default connect(null,{createStream})(formWrapped);
